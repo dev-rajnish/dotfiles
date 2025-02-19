3e45610ab387f01fd06ef2ec5be5ef0c98123860
dev-rajnish <dev.rajnish@proton.me>
2025-02-19 16:09:00 +0530

    Auto Commit and Push

--- private_dot_config/fish/functions/executables.fish
+++ private_dot_config/fish/functions/executables.fish
@@ -2,7 +2,7 @@ function executables
     #------------
     set PATH $PATH ~/.local/bin
     set PATH $PATH ~/.bun/bin
-    set PATH $PATH ~/my/pkg
+    set PATH $PATH ~/.nix-profile/bin
     set PATH $PATH ~/jupyterlab-env/bin/activate
     set PATH $PATH ~/my/ba-sh
     #-------------
