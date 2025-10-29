141c88abfbd6b905638339f57ec5c0b5db705b8f
dev-rajnish <dev.rajnish@proton.me>
2025-10-29 15:14:23 +0530

    udated kmonad script

--- dot_config/kmonad/executable_setup.sh
+++ dot_config/kmonad/executable_setup.sh
@@ -1,62 +1,5 @@
 #!/bin/bash
 
-#1 download just pkg_name folder from monarchy/dotfiles/$pkg_name in temp/
-
-# 2 move that folder to ~/.config/$pkg_name if folder exist backup name=$pkg_name.before-monarchy
-
-# 3 check for dependencies needed for $pkg_name
-## dependencies are waybar, fuzzel, hyprland, wl-clipboard, nwg-clipman,\
-##swaync, hyprshot, waypaper, swww, hyprpicker, pavucontrol, wlogout \
-## wallust, hyprlock
-
-#### ==> code
-#set -x
-
-NAME="kmonad"
-REPO="https://github.com/dev-rajnish/monarchy.git"
-TEMP="/tmp/$NAME"
-DEST="$HOME/.config/$NAME"
-#
-echo "1. Clone only waybar folder from repo"
-if [ -d "$TEMP" ]; then
-	rm -rf "$TEMP"
-fi
-
-#
-echo "2. Cloning config of $NAME ..."
-git clone --depth 1 --filter=blob:none --sparse "$REPO" "$TEMP"
-cd "$TEMP"
-git sparse-checkout set dotfiles/$NAME
-
-#
-echo "3. Setting up ~/.config/$NAME..."
-if [ -d "$DEST" ]; then
-	rm -rf "${DEST}.before-monarchy"
-	mv "$DEST" "${DEST}.before-monarchy"
-	echo "→ Backed up existing $DEST config to ~/.config/$DEST.before-monarchy"
-fi
-mv "$TEMP/dotfiles/$NAME" "$DEST"
-
-#
-echo "4. Checking dependencies..."
-REQUIRED_PKGS=(
-	kmonad
-)
-MISSING_PKGS=()
-for pkg in "${REQUIRED_PKGS[@]}"; do
-	if ! command -v "$pkg" >/dev/null 2>&1; then
-		MISSING_PKGS+=("$pkg")
-	fi
-done
-
-#
-if [ "${#MISSING_PKGS[@]}" -gt 0 ]; then
-	echo "⚠️  Missing packages: ${MISSING_PKGS[*]}"
-	echo "Please install them manually or via your package manager (YAY/pacman)"
-else
-	echo "✅ All dependencies found"
-fi
-
 sudo usermod -aG input rsh
 
 groups rsh
@@ -70,15 +13,18 @@ sudo udevadm trigger
 
 ls -l /dev/uinput
 
-sudo cp ~/.config/kmonad/kmonad.service /usr/lib/systemd/system/kmonad.service
+#sudo cp ~/.config/kmonad/kmonad.service /usr/lib/systemd/system/kmonad.service
 
 sudo mkdir -p /etc/kmonad >/dev/null
 
 sudo cp ~/.config/kmonad/config.kbd /etc/kmonad/config.kbd
-sudo systemctl enable --now kmonad
-sudo systemctl start kmonad
+sudo systemctl disable kmonad@
+sudo systemctl stop kmonad@config
+sudo systemctl enable kmonad@
+sudo systemctl start kmonad@config
 sudo systemctl daemon-reload
-
+sudo systemctl status kmonad@
+sudo systemctl status kmonad@config
 #
 echo "✅ $NAME setup complete under 'monarchy'"
 #
