963807feeaffc0899129261515ad63077978e041
dev-rajnish <dev.rajnish@proton.me>
2025-11-05 17:04:56 +0530

    add wallpapers

--- dot_config/home-manager/home.nix
+++ dot_config/home-manager/home.nix
@@ -15,6 +15,7 @@
   nwg-look
   ncpamixer
   gemini-cli-bin
+  just
   ];
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
